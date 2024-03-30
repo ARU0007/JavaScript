b4a82c57e3653bdc50b7ffd181dde5dc195cb79d
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-30 21:40:38 +0530

    added boolean variable

--- variable.js
+++ variable.js
@@ -1,6 +1,9 @@
+// JavaScript is Dynamic language.
+
 book1 = "Ocean peace";
 book1_price = 7.99; //$
 book1_QTY = 8;
 
+book1_is_good = true;
 book1_lost_QTY = null;
 book1_author_name = undefined;
