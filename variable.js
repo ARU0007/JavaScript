13ce39e345028a923d55f9632b7ca44a215cdde0
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-30 21:46:22 +0530

    added comment rules

--- variable.js
+++ variable.js
@@ -1,4 +1,12 @@
 // JavaScript is Dynamic language.
+/*  RULES:-
+Variable names are case sensitive; "a" & "A" is different.
+
+Only letters, digits, underscore(_) and $ is allowed. (not even space)
+
+Only a letter, underscore(_) or $ should be 1st character.
+
+Reserved words cannot be variable names. */
 
 book1 = "Ocean peace";
 book1_price = 7.99; //$
