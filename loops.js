c632ef74bb7bc0b940e0605553e5a5530b9abe0f
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-02 23:27:52 +0530

    created do while

--- loops.js
+++ loops.js
@@ -3,3 +3,12 @@ let str = "Hello, World!";
 for (let i = 0; i < str.length; i++) {
   console.log(str[i]);
 }
+
+//while
+let str2 = "Green , Rainbow";
+let i = 0;
+
+while (i < str2.length) {
+  console.log(str2[i]);
+  i++;
+}
