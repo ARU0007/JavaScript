93b126c81e41e38d6e710dc1bcbeef8f78b0098c
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-02 23:31:35 +0530

    created for of loop

--- loops.js
+++ loops.js
@@ -4,11 +4,26 @@ for (let i = 0; i < str.length; i++) {
   console.log(str[i]);
 }
 
-//while
+//while loop
 let str2 = "Green , Rainbow";
 let i = 0;
 
 while (i < str2.length) {
   console.log(str2[i]);
   i++;
+}
+
+//do while loop
+let j = 0;
+
+do {
+  console.log("The value of i is: " + j);
+  j++;
+} while (j < 5);
+
+// for of loop
+let str3 = "Hello, World!";
+
+for (let char of str3) {
+  console.log(char);
 }
