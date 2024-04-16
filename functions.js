72a80ad075a9ecc6d22c5b0b4e8fdc31fffa6fbf
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-16 18:31:24 +0530

    created first function

---
+++ functions.js
@@ -0,0 +1,16 @@
+/* 
+Syntax:-
+
+function functionName(p1,p2,.....){
+    .............
+    .............
+}
+ */
+
+function myFunction1(msg) {
+  //parameter(input).
+  console.log(msg);
+}
+
+//function call
+myFunction1("I Love Birds");
