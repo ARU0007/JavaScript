ff355572789bf114e68fa86548699ca836ad0a16
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-16 19:00:22 +0530

    created first arrow function.

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -2,4 +2,8 @@
 const functionName = (p1,p2,....)=>{......}
 */
 
-const arrowMultiplication = (x, y) => {};
+const arrowMultiplication = (x, y) => {
+  console.log(x * y);
+};
+
+arrowMultiplication(3 * 7);
