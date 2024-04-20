805180ed9d8293ad568ae0afb5ac2b14346fac3e
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-20 14:47:36 +0530

    some more methods

--- dom.js
+++ dom.js
@@ -2,3 +2,16 @@
 
 let heading = document.getElementById("heading");
 console.dir(heading);
+
+let repeatedButton = document.getElementsByClassName("repeatedButton");
+console.dir(repeatedButton);
+
+let paras = document.getElementsByTagName("p");
+console.dir(paras);
+
+//query selector
+let firstElement = document.querySelector("p");
+console.dir(firstElement);
+
+let allElement = document.querySelectorAll("p");
+console.dir(allElement);
